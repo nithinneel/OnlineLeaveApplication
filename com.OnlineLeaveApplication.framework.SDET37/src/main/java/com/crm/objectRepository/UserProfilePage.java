ddbc5ffbabf16256e9f659db4cb1331140748ee3
nithinneel <nithinneelgunda@gmail.com>
2022-08-03 16:34:41 +0530

    qwerty

---
+++ com.OnlineLeaveApplication.framework.SDET37/src/main/java/com/crm/objectRepository/UserProfilePage.java
@@ -0,0 +1,41 @@
+package com.crm.objectRepository;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+
+/**
+ * 
+ * @author nithin
+ *
+ */
+public class UserProfilePage {
+	//initialization
+	public UserProfilePage(WebDriver driver) {
+		PageFactory.initElements(driver, this);
+	}
+	
+	//declaration
+	@FindBy(name = "userPersonalPhoneNumber") private WebElement userPersonalPhoneNumber;
+	@FindBy(name = "userPresentAddress") private WebElement userPresentAddress;
+	@FindBy(name = "btnUserInfoUpdate") private WebElement submitBtn;
+	
+	
+	public WebElement getUserPersonalPhoneNumber() {
+		return userPersonalPhoneNumber;
+	}
+	public WebElement getUserPresentAddress() {
+		return userPresentAddress;
+	}
+	public WebElement getSubmitBtn() {
+		return submitBtn;
+	}
+	
+	public void AdminUpdateProfile(String personalPhoneNumber, String presentAddress) {
+		userPersonalPhoneNumber.sendKeys(personalPhoneNumber);
+		userPresentAddress.sendKeys(presentAddress);
+		submitBtn.click();
+	}
+	
+}
