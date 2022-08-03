df818e8973805c7b72b5888f4024d2cde36a977d
AnupOmm123 <anupamaxe94@yahoo.com>
2022-08-03 18:38:42 +0530

    new

--- com.OnlineLeaveApplication.framework.SDET37/src/main/java/com/crm/objectRepository/GoToApplicationPage.java
+++ com.OnlineLeaveApplication.framework.SDET37/src/main/java/com/crm/objectRepository/GoToApplicationPage.java
@@ -7,7 +7,7 @@ import org.openqa.selenium.support.PageFactory;
 
 public class GoToApplicationPage {
 
-	//initialisation
+	//initialisationn
 	public GoToApplicationPage(WebDriver driver)
 	{ PageFactory.initElements(driver, this);
 	
