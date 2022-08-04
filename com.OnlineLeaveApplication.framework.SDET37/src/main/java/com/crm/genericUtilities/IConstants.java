67516fc7ff286859b2e07731f60edac224b4a9b0
ShubhGithub1952 <pitleshubham14@gmail.com>
2022-08-04 10:40:59 +0530

    in IConstant i have Added GeckoDriver Executable file Path and Remaining properties as well as Excel File also

--- com.OnlineLeaveApplication.framework.SDET37/src/main/java/com/crm/genericUtilities/IConstants.java
+++ com.OnlineLeaveApplication.framework.SDET37/src/main/java/com/crm/genericUtilities/IConstants.java
@@ -2,9 +2,10 @@ package com.crm.genericUtilities;
 
 public interface IConstants 
 {
-	String filePath="./src/test/resources/commondata.properties.txt";
-	String excelPath="./src/test/resources/Sdet37TestData.xlsx";
+	String filePath="./src/test/resources/commonDataOLMT.properties";
+	String excelPath="./src/test/resources/OlmEmployeeData.xlsx";
 	String chromePath="./src/main/resources/chromedriver.exe";
+	String  firefoxPath="/src/main/resources/geckodriver.exe";
 	String chromeKey="webdriver.chrome.driver";
 	String DbUrl="jdbc:mysql://localhost:3306/";
 	String DBUsername="root";
