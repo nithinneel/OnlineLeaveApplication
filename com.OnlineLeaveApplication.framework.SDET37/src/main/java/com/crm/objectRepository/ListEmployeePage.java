c46a4d523eabc0dd804bbdcacbc605dd7120820b
nithinneel <105333944+nithinneel@users.noreply.github.com>
2022-08-10 16:00:26 +0530

    Update ListEmployeePage.java

--- com.OnlineLeaveApplication.framework.SDET37/src/main/java/com/crm/objectRepository/ListEmployeePage.java
+++ com.OnlineLeaveApplication.framework.SDET37/src/main/java/com/crm/objectRepository/ListEmployeePage.java
@@ -6,26 +6,6 @@ import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
 
 import com.crm.genericUtilities.WebDriverUtility;
-<<<<<<< HEAD
-//intialization
-public class ListEmployeePage extends WebDriverUtility {
-public ListEmployeePage(WebDriver driver) {
-	PageFactory.initElements(driver, this);
-}
-//declairation
-@FindBy(id = "empDptName") 
-private WebElement dropdown;
-
-public WebElement getdropdown() {
-	return dropdown;
-}
-//utilasation
-public void selectDropdown(String visible) {
-selectDropDownByVtext(dropdown, visible);
-}
-}
-=======
-
 public class ListEmployeePage extends WebDriverUtility{
 	
 	//initialization
