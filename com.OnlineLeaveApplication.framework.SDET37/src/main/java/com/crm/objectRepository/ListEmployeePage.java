7e3dc15d9043c55a2a78871ca3d2397090d37335
91900 <ggiridhar91@gmail.com>
2022-08-11 16:34:57 +0530

    tc2

--- com.OnlineLeaveApplication.framework.SDET37/src/main/java/com/crm/objectRepository/ListEmployeePage.java
+++ com.OnlineLeaveApplication.framework.SDET37/src/main/java/com/crm/objectRepository/ListEmployeePage.java
@@ -1,17 +1,20 @@
 package com.crm.objectRepository;
 
+import javax.management.loading.PrivateClassLoader;
+
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
 
 import com.crm.genericUtilities.WebDriverUtility;
+import com.github.dockerjava.api.model.Driver;
 public class ListEmployeePage extends WebDriverUtility{
 	
 	//initialization
 	public ListEmployeePage(WebDriver driver) {
 		PageFactory.initElements(driver, this);
-		
 	
 	}
 	//declaration
@@ -21,7 +24,16 @@ public class ListEmployeePage extends WebDriverUtility{
 	}
 	
 	public void selectDepartment(String department) {
-		selectDropDown(deptDropDown, department);
+		selectDropDownByVtext(deptDropDown, department);
 	}
-}
+	
+	public void clickOnActiveButton(WebDriver driver, String empID) {
+		WebElement activeButton = driver.findElement(By.xpath("//a[.='"+empID+"']/../..//button[@name='btnVerify']"));
+	    activeButton.click();	 
+	}
+	public void clickOnEmpCode(WebDriver driver, String empID) {
+	WebElement empcode = driver.findElement(By.xpath("//a[.='"+empID+"']"));
+	empcode.click();
+	}
+	}
 
